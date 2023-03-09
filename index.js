a311175ff45804df76e2826f60eb43655c6f6b15
Bella <bella.kamala@gmail.com>
2023-03-10 01:34:39 +0300

    added solutions

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 const filterByAge = (el) => {
+    return el.age < 50
 
 };
 
 const filterByScore = (el) => {
-
+    return el.score > 75
 };
 
 const filterByServiceRecord = (el) => {
-
+    return el.serviceRecord > 5
 };
 
 const sortByScore = (arr) => {
-
+    const newArr = [...arr]
+    return newArr.sort((a, b) =>
+        b.score - a.score
+    );
 };
 
 const getMeanAge = (arr) => {
+
+    let sum = 0;
+    let n = arr.length
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].age
+    }
+    return sum / n
 
 };
 
 const getMeanAgeReduce = (arr) => {
 
-};
+    let sum = arr.reduce((n, { age }) => n + age, 0)
+    return sum / arr.length
+
+}
+
+
 
 const getNamesOnly = (arr) => {
+
+    return arr.name
 
 };
 
 module.exports = {
-filterByAge,
-filterByScore,
-filterByServiceRecord,
-sortByScore,
-getMeanAge,
-getMeanAgeReduce,
-getNamesOnly,
-};
+    filterByAge,
+    filterByScore,
+    filterByServiceRecord,
+    sortByScore,
+    getMeanAge,
+    getMeanAgeReduce,
+    getNamesOnly,
+}
